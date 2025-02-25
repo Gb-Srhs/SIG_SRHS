e25e32f0d48decef7b8159df4e83716116909e15
JBPX <gabrielsantosdemoura93@gmail.com>
2025-02-25 16:47:50 -0300

    Adicionando novas camadas - 25/02
    Feliz ano novo

--- js/labels.js
+++ js/labels.js
@@ -1,11 +1,13 @@
 var hideLabel = function(label) {
-    label.labelObject.style.opacity = 0;
-    label.labelObject.style.transition = 'opacity 0s';
+  label.labelObject.style.opacity = 0;
+  label.labelObject.style.transition = 'opacity 0s';
 };
+
 var showLabel = function(label) {
-    label.labelObject.style.opacity = 1;
-    label.labelObject.style.transition = 'opacity 1s';
+  label.labelObject.style.opacity = 1;
+  label.labelObject.style.transition = 'opacity 1s';
 };
+
 labelEngine = new labelgun.default(hideLabel, showLabel);
 
 var id = 0;
@@ -13,48 +15,50 @@ var labels = [];
 var totalMarkers = 0;
 
 function resetLabels(markers) {
-    labelEngine.reset();
-    var i = 0;
-    for (var j = 0; j < markers.length; j++) {
-        markers[j].eachLayer(function(label){
-            addLabel(label, ++i);
-        });
-    }
+  labelEngine.reset();
+  var i = 0;
+  for (var j = 0; j < markers.length; j++) {
+      markers[j].eachLayer(function(layer){
+          addLabel(layer, ++i);
+      });
+  }
   labelEngine.update();
 }
 
 function addLabel(layer, id) {
-    // Verifica se o layer tem um tooltip antes de continuar
-    if (layer.getTooltip && layer.getTooltip() && layer.getTooltip()._source && layer.getTooltip()._source._tooltip && layer.getTooltip()._source._tooltip._container) {
-        var label = layer.getTooltip()._source._tooltip._container;
-        
-        if (label) {
-            var rect = label.getBoundingClientRect();
+  // Verifica se o Tooltip está disponível
+  if (layer.getTooltip() && layer.getTooltip()._source && layer.getTooltip()._source._tooltip) {
+      var tooltip = layer.getTooltip()._source._tooltip;
+      var label = tooltip._container;
 
-            var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
-            var topRight = map.containerPointToLatLng([rect.right, rect.top]);
-            var boundingBox = {
-                bottomLeft: [bottomLeft.lng, bottomLeft.lat],
-                topRight: [topRight.lng, topRight.lat]
-            };
+      // Certifica-se de que o container do tooltip existe antes de prosseguir
+      if (label) {
+          // Obtém o retângulo delimitador do próprio rótulo
+          var rect = label.getBoundingClientRect();
 
-            labelEngine.ingestLabel(
-                boundingBox,
-                id,
-                parseInt(Math.random() * (5 - 1) + 1), // Peso
-                label,
-                "Test " + id,
-                false
-            );
+          // Converte as coordenadas do container (em espaço de tela) para Lat/Lng
+          var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
+          var topRight = map.containerPointToLatLng([rect.right, rect.top]);
+          var boundingBox = {
+              bottomLeft : [bottomLeft.lng, bottomLeft.lat],
+              topRight   : [topRight.lng, topRight.lat]
+          };
 
-            if (!layer.added) {
-                layer.addTo(map);
-                layer.added = true;
-            }
-        } else {
-            console.error("Label is undefined.");
-        }
-    } else {
-        console.error("Tooltip or related properties are undefined for this layer.");
-    }
+          // Ingesta o rótulo no labelgun
+          labelEngine.ingestLabel(
+              boundingBox,
+              id,
+              parseInt(Math.random() * (5 - 1) + 1), // Peso
+              label,
+              "Test " + id,
+              false
+          );
+
+          // Verifica se a camada ainda não foi adicionada ao mapa
+          if (!layer.added) {
+              layer.addTo(map);
+              layer.added = true;
+          }
+      }
+  }
 }
