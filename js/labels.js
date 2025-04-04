75a74c48c5a43d5916b1897a05b5bfc713a79de4
JBPX <gabrielsantosdemoura93@gmail.com>
2025-04-04 10:24:55 -0300

    Municipios gráficos interativos
    Painés com Atributos:
    
    - Municipios (substituído por nova camada)
    - Municipios PB
    - Unidades de Planejamento
    
    Obs: Tentar alterar por código o padrão de ativado/desativado das camadas

--- js/labels.js
+++ js/labels.js
@@ -1,13 +1,11 @@
 var hideLabel = function(label) {
-  label.labelObject.style.opacity = 0;
-  label.labelObject.style.transition = 'opacity 0s';
+    label.labelObject.style.opacity = 0;
+    label.labelObject.style.transition = 'opacity 0s';
 };
-
 var showLabel = function(label) {
-  label.labelObject.style.opacity = 1;
-  label.labelObject.style.transition = 'opacity 1s';
+    label.labelObject.style.opacity = 1;
+    label.labelObject.style.transition = 'opacity 1s';
 };
-
 labelEngine = new labelgun.default(hideLabel, showLabel);
 
 var id = 0;
@@ -15,50 +13,50 @@ var labels = [];
 var totalMarkers = 0;
 
 function resetLabels(markers) {
-  labelEngine.reset();
-  var i = 0;
-  for (var j = 0; j < markers.length; j++) {
-      markers[j].eachLayer(function(layer){
-          addLabel(layer, ++i);
-      });
-  }
+    labelEngine.reset();
+    var i = 0;
+    for (var j = 0; j < markers.length; j++) {
+        markers[j].eachLayer(function(label){
+            addLabel(label, ++i);
+        });
+    }
   labelEngine.update();
 }
 
 function addLabel(layer, id) {
-  // Verifica se o Tooltip está disponível
-  if (layer.getTooltip() && layer.getTooltip()._source && layer.getTooltip()._source._tooltip) {
-      var tooltip = layer.getTooltip()._source._tooltip;
-      var label = tooltip._container;
 
-      // Certifica-se de que o container do tooltip existe antes de prosseguir
+  // This is ugly but there is no getContainer method on the tooltip :(
+  if (layer.getTooltip()) {
+      var label = layer.getTooltip()._source._tooltip._container;
       if (label) {
-          // Obtém o retângulo delimitador do próprio rótulo
-          var rect = label.getBoundingClientRect();
 
-          // Converte as coordenadas do container (em espaço de tela) para Lat/Lng
-          var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
-          var topRight = map.containerPointToLatLng([rect.right, rect.top]);
-          var boundingBox = {
-              bottomLeft : [bottomLeft.lng, bottomLeft.lat],
-              topRight   : [topRight.lng, topRight.lat]
-          };
+        // We need the bounding rectangle of the label itself
+        var rect = label.getBoundingClientRect();
 
-          // Ingesta o rótulo no labelgun
-          labelEngine.ingestLabel(
-              boundingBox,
-              id,
-              parseInt(Math.random() * (5 - 1) + 1), // Peso
-              label,
-              "Test " + id,
-              false
-          );
+        // We convert the container coordinates (screen space) to Lat/lng
+        var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
+        var topRight = map.containerPointToLatLng([rect.right, rect.top]);
+        var boundingBox = {
+          bottomLeft : [bottomLeft.lng, bottomLeft.lat],
+          topRight   : [topRight.lng, topRight.lat]
+        };
 
-          // Verifica se a camada ainda não foi adicionada ao mapa
-          if (!layer.added) {
-              layer.addTo(map);
-              layer.added = true;
-          }
+        // Ingest the label into labelgun itself
+        labelEngine.ingestLabel(
+          boundingBox,
+          id,
+          parseInt(Math.random() * (5 - 1) + 1), // Weight
+          label,
+          "Test " + id,
+          false
+        );
+
+        // If the label hasn't been added to the map already
+        // add it and set the added flag to true
+        if (!layer.added) {
+          layer.addTo(map);
+          layer.added = true;
+        }
       }
   }
 }
