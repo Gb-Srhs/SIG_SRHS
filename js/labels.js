e9473b9418593988bd4d10c33d10c658c572adff
JBPX <gabrielsantosdemoura93@gmail.com>
2025-04-29 10:13:51 -0300

    Fix - Versao Funcional v1
    - Mudanças no codigo labels.js, substituindo por uma versão que "pula" os tooltips e causa problemas
    
    - Talvez modificar um pouco para deixar os labels um pouco mais "organizados" quando mostrados, mas por agora estou salvando isso por ser funcional e resolver o centro do problema.

--- js/labels.js
+++ js/labels.js
@@ -1,11 +1,13 @@
 var hideLabel = function(label) {
-    label.labelObject.style.opacity = 0;
-    label.labelObject.style.transition = 'opacity 0s';
+  label.labelObject.style.opacity = 0;
+  label.labelObject.style.transition = 'opacity 0s';
 };
+
 var showLabel = function(label) {
-    label.labelObject.style.opacity = 1;
-    label.labelObject.style.transition = 'opacity 1s';
+  label.labelObject.style.opacity = 1;
+  label.labelObject.style.transition = 'opacity 1s';
 };
+
 labelEngine = new labelgun.default(hideLabel, showLabel);
 
 var id = 0;
@@ -13,50 +15,43 @@ var labels = [];
 var totalMarkers = 0;
 
 function resetLabels(markers) {
-    labelEngine.reset();
-    var i = 0;
-    for (var j = 0; j < markers.length; j++) {
-        markers[j].eachLayer(function(label){
-            addLabel(label, ++i);
-        });
-    }
+  labelEngine.reset();
+  var i = 0;
+  for (var j = 0; j < markers.length; j++) {
+      markers[j].eachLayer(function(label){
+          addLabel(label, ++i);
+      });
+  }
   labelEngine.update();
 }
 
 function addLabel(layer, id) {
+  const tooltip = layer.getTooltip?.();
+  const tooltipContainer = tooltip?._source?._tooltip?._container;
 
-  // This is ugly but there is no getContainer method on the tooltip :(
-  if (layer.getTooltip()) {
-      var label = layer.getTooltip()._source._tooltip._container;
-      if (label) {
+  if (tooltipContainer) {
+      const rect = tooltipContainer.getBoundingClientRect();
 
-        // We need the bounding rectangle of the label itself
-        var rect = label.getBoundingClientRect();
+      const bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
+      const topRight = map.containerPointToLatLng([rect.right, rect.top]);
 
-        // We convert the container coordinates (screen space) to Lat/lng
-        var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
-        var topRight = map.containerPointToLatLng([rect.right, rect.top]);
-        var boundingBox = {
+      const boundingBox = {
           bottomLeft : [bottomLeft.lng, bottomLeft.lat],
           topRight   : [topRight.lng, topRight.lat]
-        };
+      };
 
-        // Ingest the label into labelgun itself
-        labelEngine.ingestLabel(
+      labelEngine.ingestLabel(
           boundingBox,
           id,
-          parseInt(Math.random() * (5 - 1) + 1), // Weight
-          label,
+          parseInt(Math.random() * (5 - 1) + 1), // Random weight between 1 and 5
+          tooltipContainer,
           "Test " + id,
           false
-        );
+      );
 
-        // If the label hasn't been added to the map already
-        // add it and set the added flag to true
-        if (!layer.added) {
+      if (!layer.added) {
           layer.addTo(map);
           layer.added = true;
-        }
       }
   }
 }
